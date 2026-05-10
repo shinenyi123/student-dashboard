768234a007668832f7432d3c00a3b2afa9e738e9
shinenyi123 <shinenyinyiaung83@gmail.com>
2026-05-10 08:00:12 +0630

    Update app.py

--- app.py
+++ app.py
@@ -273,7 +273,7 @@ def view_data():
             ws.column_dimensions['H'].width = 20 #မှတ်ချက်
 
             for row in ws.iter_rows():
-                ws.row_dimensions[row[0].row].height = 22
+                ws.row_dimensions[row[0].row].height = 21
 
             for row in ws.iter_rows(min_row=6):
 
