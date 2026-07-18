c8db88a7a00c9d4ab3adac830e0a64a41f78c90d
Shine Nyi Nyi Aung <shinenyinyiaung83@gmail.com>
2026-07-18 06:39:29 +0630

    Update app.py

--- app.py
+++ app.py
@@ -396,5 +396,5 @@ def view_data():
 
     return render_template('try.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
